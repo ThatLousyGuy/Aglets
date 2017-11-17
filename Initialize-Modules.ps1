caab082fcd1a0296b9a7dbb26327972ce330fd5d
ThatLousyGuy <thatlousyguy@outlook.com>
2017-11-17 02:35:01 -0800

    Add functions for paths' parent directories
    - Added 'startp' for opening the parent directory
    - Added 'clipp' for copying the parent directory to the clipboard

--- Initialize-Modules.ps1
+++ Initialize-Modules.ps1
@@ -8,3 +8,17 @@ Function Open-BookmarkFromGlobalFiles
     Open-BookmarkFromFiles $Global:BookmarksFiles $Alias
 }
 Set-Alias b Open-BookmarkFromGlobalFiles
+
+Function Get-ParentDirectory
+{
+    param([string]$Path)
+    Set-Clipboard (Split-Path -Parent $Path)
+}
+Set-Alias clipp Get-ParentDirectory
+
+Function Start-ParentDirectory
+{
+    param([string]$Path)
+    start (Split-Path -Parent $Path)
+}
+Set-Alias startp Start-ParentDirectory
--- modules/ClipModule.psm1
+++ modules/ClipModule.psm1
@@ -1,6 +1,6 @@
 # Newline-less implementation borrowed from StackOverflow:
 # https://stackoverflow.com/questions/1567112/convert-keith-hills-powershell-get-clipboard-and-set-clipboard-to-a-psm1-script
-function Get-ClipBoard {
+Function Get-ClipBoard {
     Add-Type -AssemblyName System.Windows.Forms
     $tb = New-Object System.Windows.Forms.TextBox
     $tb.Multiline = $true
@@ -9,7 +9,7 @@ function Get-ClipBoard {
 }
 Set-Alias clipped Get-ClipBoard
 
-function Set-ClipBoard() {
+Function Set-ClipBoard {
     Param(
       [Parameter(ValueFromPipeline=$true)]
       [string] $text
